954413ff05647a0613a71b2141828c306387794d
Divya Sirala <divyasirala@gmail.com>
2026-06-20 22:07:54 +0530

    Containerize with docker-compose

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./sqllite.db"
+import os
+
+#Useful for local development and deployment using docker
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./data/sqllite.db")
 
 engine = create_engine(
     DATABASE_URL,
